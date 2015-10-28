ea1c716094f3c220831c76761ddd318bcaf9f4a7
Robrecht <robrecht.rikken@student.kdg.be>
2015-10-28 19:46:08 +0100

    Opdrachten toegevoegd aan opdracht 2. Scorebord begonnen Chessgame

---
+++ -Opdracht-03-Versiebeheer/-opdracht in team werken/SchakenGame/ChessGameRR/ChessGameRR/ScoreBord.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace ChessGameRR
+{
+    partial class ScoreBord
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBlack = new System.Windows.Forms.Label();
+            this.lblWhite = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblBlack
+            // 
+            this.lblBlack.AutoSize = true;
+            this.lblBlack.Location = new System.Drawing.Point(21, 15);
+            this.lblBlack.Name = "lblBlack";
+            this.lblBlack.Size = new System.Drawing.Size(53, 13);
+            this.lblBlack.TabIndex = 0;
+            this.lblBlack.Text = "Black lost";
+            // 
+            // lblWhite
+            // 
+            this.lblWhite.AutoSize = true;
+            this.lblWhite.Location = new System.Drawing.Point(24, 372);
+            this.lblWhite.Name = "lblWhite";
+            this.lblWhite.Size = new System.Drawing.Size(54, 13);
+            this.lblWhite.TabIndex = 1;
+            this.lblWhite.Text = "White lost";
+            // 
+            // ScoreBord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblWhite);
+            this.Controls.Add(this.lblBlack);
+            this.Name = "ScoreBord";
+            this.Size = new System.Drawing.Size(189, 559);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBlack;
+        private System.Windows.Forms.Label lblWhite;
+    }
+}
